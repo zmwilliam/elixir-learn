b680ae2a7e60b1a524ef1d86d2d537e845920378
zmwilliam <willcxa@gmail.com>
2016-03-15 07:46:44 -0300

    Module refactoring

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,19 +1,12 @@
 defmodule FizzBuzz do
 
-  def printIt(number) when rem(number, 3) == 0 and rem(number, 5) == 0 do
-    "FizzBuzz"
-  end
-
-  def printIt(number) when rem(number, 3) == 0 do
-    "Fizz"
-  end
-
-  def printIt(number) when rem(number, 5) == 0 do
-    "Buzz"
-  end
-
   def printIt(number) do
-    number |> to_string
+    doFB(rem(number, 3), rem(number, 5), number)
   end
+
+  defp doFB(0,0,_) do "FizzBuzz" end
+  defp doFB(0,_,_) do "Fizz" end
+  defp doFB(_,0,_) do "Buzz" end
+  defp doFB(_, _, number) do number |> to_string end
 
 end
